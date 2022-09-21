3f5f569cb87760fbcac39450df0f14ff0fa4f8dc
Developer3-Emmanuel <helloteam.developer3@gmail.com>
2022-09-21 12:07:12 -0500

    Se agrega la ruta confirmacorreo

--- src/router/index.js
+++ src/router/index.js
@@ -26,6 +26,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/RegistroView.vue'),
   },
+  {
+    path: '/confirmacorreo',
+    name: 'confirmacorreo',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/ConfirmaCorreoView.vue'),
+  },
 ];
 
 const router = new VueRouter({
